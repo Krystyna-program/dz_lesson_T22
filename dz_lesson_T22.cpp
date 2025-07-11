92ec33933c1c927562848b353988c69f771dec20
Krystyna Khondak <kristinakhondak@gmail.com>
2025-07-11 21:17:16 +0300

    Search by name

--- dz_lesson_T22.cpp
+++ dz_lesson_T22.cpp
@@ -1,22 +1,42 @@
 #include <vector>
 #include <algorithm>
 #include "Computer.h"
-using namespace std;
 
 int main()
 {
-	vector<Computer> comp;
+    vector<Computer> comp;
 
-	Computer a("Lenovo", 3.5, 16, true, 18000);
-	Computer b("Asus", 2.8, 8, false, 14500);
+    comp.push_back(Computer("Lenovo", 3.5, 16, true, 18000));
+    comp.push_back(Computer("Asus", 2.8, 8, false, 14500));
+    comp.push_back(Computer("HP", 3.0, 32, true, 23000));
 
-	comp.push_back(a); // size = 1
-	comp.push_back(b); // size = 2
-	comp.push_back(Computer("HP", 3.0, 32, true, 23000));
+    cout << "=== List of Computers ===\n";
+    for (int i = 0; i < comp.size(); ++i) {
+        comp[i].showInfo();
+    }
+    cout << "===================================\n";
 
-	for (int i = 0; i < comp.size(); i++)
-	{
-		comp[i].showInfo();
-	}
-	cout << "===================================\n";
+    // 1
+    char user[100];
+    cout << "Input title: ";
+    cin.getline(user, 100);
+
+    auto result = find_if(comp.begin(), comp.end(), [user](const Computer& obj) {
+        return strcmp(user, obj.getTitle()) == 0;
+        });
+
+    if (result == comp.end()) {
+        cout << "Not found!\n";
+    }
+    else {
+        cout << "\n=== Found Computer ===\n";
+        result->showInfo();
+        comp.erase(result);
+
+        cout << "\n=== Updated List ===\n";
+        for (int i = 0; i < comp.size(); ++i) {
+            comp[i].showInfo();
+        }
+    }
+    cout << "===================================\n";
 }
